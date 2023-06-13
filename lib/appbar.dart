e5b91e78d54a8bc5a7603407a2d13a8b8c5b460d
MuradRahmanSaud <murad.rahman.saud@gmail.com>
2023-06-13 15:20:06 +0600

    V3

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practice/search_Page.dart';
 
 class App_Bar extends StatelessWidget {
   const App_Bar({super.key});
@@ -7,29 +8,12 @@ class App_Bar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [Icon(Icons.mail)],
-        title: TextField(
-          style: TextStyle(color: Colors.white),
-          decoration: InputDecoration(
-
-            hintText: "Search ...",
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(5),
-              borderSide: BorderSide.none,
-              gapPadding: 5,
-            ),
-            hoverColor: Colors.white,
-            hintStyle: TextStyle(
-              color: Colors.teal,
-            ),
-            prefixIcon: Icon(
-              Icons.search,
-              color: Colors.orange,
-            ),
-            filled: true,
-            fillColor: Colors.white,
-          ),
-        ),
+        title: Text("Search"),
+        actions: [
+          IconButton(onPressed: () => Navigator.of(context)
+              .push(MaterialPageRoute(builder: (_) => const SearchPage())),
+              icon: Icon(Icons.search))
+        ],
       ),
     );
   }
