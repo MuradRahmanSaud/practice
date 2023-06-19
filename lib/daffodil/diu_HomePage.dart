02ca9b4df7ee0ccd1601485a2a546363d0074ce6
MuradRahmanSaud <murad.rahman.saud@gmail.com>
2023-06-19 11:51:00 +0600

    Solved SingleChildScrollView

--- lib/daffodil/diu_HomePage.dart
+++ lib/daffodil/diu_HomePage.dart
@@ -7,7 +7,7 @@ class DIU_HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Daffodil International Universityy'),
+          title: Text('Daffodil International University'),
         ),
         body: Column(
           children: [
